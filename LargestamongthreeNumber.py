f16cf2484b094f20d0351ac4796e62a3a1dddd54
VanshikaSharma07 <vanshika.sharma@janbask.com>
2025-02-21 09:04:17 +0530

    largeNumber

---
+++ LargestamongthreeNumber.py
@@ -0,0 +1,9 @@
+num1=int(input("Enter the value of num1 :"))
+num2=int(input("Enter the value of num2 :"))
+num3=int(input("Enter the value of num3 :"))
+if(num1>num2 and num1>num3):
+    print(num1,"num1 is greater")
+elif(num2>num3):
+    print(num2,"num 2is greater")
+else:
+    print(num3,"num 3is greater")
